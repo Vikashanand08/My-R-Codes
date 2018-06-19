40648fb7b90ca773ac39a0915e0c836b8719c8a7
Vikash Anand <31685541+Vikashanand08@users.noreply.github.com>
2018-06-19 09:38:21 +0530

    Gujarat districts and talukas

--- Administrative Maps of India.R
+++ Administrative Maps of India.R
@@ -1,6 +1,8 @@
 
 # Load raster package
 library(raster)
+library(sp)
+library(RColorBrewer)
 
 # Map of India
 # Downlaod data from GADM website i.e. https://gadm.org/download_country_v3.html
@@ -13,8 +15,8 @@ plot(india1,border="blue")
 
 
 # Map of India with State and District Borders
-india3<-readRDS("gadm36_IND_2_sp.rds")
-plot(india3,border="blue")
+india2<-readRDS("gadm36_IND_2_sp.rds")
+plot(india2,border="blue")
 
 # Map of India with State, District and Taluka Level
 india3<-readRDS("gadm36_IND_3_sp.rds")
@@ -25,3 +27,78 @@ india3<-readRDS("gadm36_IND_3_sp.rds")
 plot(india3,border="blue")+
   title(main = "Map of India with State, District 
         and Taluka boundary",sub = "Source GADM Data")
+
+# Map of United States
+usa0<-readRDS("gadm36_USA_0_sp.rds")
+plot(usa0,border="blue")
+
+# Map of United States with State Boundry
+usa1<-readRDS("gadm36_USA_1_sp.rds")
+plot(usa1,border="blue")+title(main = "United States", sub = "Source GADM")
+
+
+# Administrative Map of India
+india_states<-readRDS("gadm36_IND_1_sp.rds")
+n<-length(india_states$NAME_1)
+plot(india_states,col=rainbow(n),main="Administartive Map of India")
+text(india_states,india_states$NAME_1)
+
+# Boundrymap of India with longitude and latitude as axes
+plot(india0,border="red",lwd=3,axes=TRUE)
+
+# Map of India with States colour
+plot(india1,border="black",lwd=2,axes=2)
+india1$NAME_1=as.factor(india1$NAME_1)
+india1$fake.data=runif(length(india1$NAME_1))
+spplot(india1,"NAME_1",col.regions=rgb(0,india1$fake.data,0),
+       colorkey=T,main="States of India")
+
+
+# Map of Gujarat in Blue colour
+gujarat1 = (india1[india1$NAME_1=="Gujarat",])
+spplot(gujarat1,"NAME_1", col.regions=rgb(0,0,1), 
+       main = "Gujarat, India",scales=list(draw=T), colorkey =F)
+ 
+# Map of Gujarat in Red colour
+gujarat2 = (india1[india1$NAME_1=="Gujarat",])
+spplot(gujarat2,"NAME_1", col.regions=rgb(1,0,0), 
+       main = "Gujarat, India",scales=list(draw=T), colorkey =F)     
+
+
+# Map of Gujarat in Green colour without district borders
+gujarat3 = (india1[india1$NAME_1=="Gujarat",])
+spplot(gujarat3,"NAME_1", col.regions=rgb(0,1,0), 
+       main = "Gujarat, India",scales=list(draw=T), colorkey =F) 
+
+
+# Map of Gujarat with district borders
+gujarat4 = (india2[india2$NAME_1=="Gujarat",])
+spplot(gujarat4,"NAME_1", main = "Gujarat Districts", colorkey =F)
+
+# Map of Gujarat with districts coloured with different districts
+gujarat4$NAME_2=as.factor(gujarat4$NAME_2)
+col=rainbow(length(levels(gujarat4$NAME_2)))
+spplot(gujarat4,"NAME_2",col.regions=col,colorkey=T)
+
+# Map of Gujarat with district and taluka borders
+gujarat5 = (india3[india3$NAME_2=="Gujarat",])
+spplot(gujarat4,"NAME_2", main = "Gujarat Districts ", colorkey =F)
+
+
+
+# Map of Gujarat with districts coloured with different districts and taluka border
+gujarat4$NAME_2=as.factor(gujarat4$NAME_2)
+col=rainbow(length(levels(gujarat4$NAME_2)))
+spplot(gujarat4,"NAME_2",col.regions=col,colorkey=T)
+
+
+
+
+
+
+
+# Map of Gandhinagar, Gujarat with taluka borders needs to be corrected
+gujarat5 = (india3[india3$NAME_1=="Gujarat",])
+gandhinagar = (gujarat5[gujarat5$NAME_2=="Gandhinagar",])
+spplot(gandhinagar,border="red")
+
---
+++ Gujarat districts and talukas.R
@@ -0,0 +1,83 @@
+library(sp)
+library(RColorBrewer)
+
+# Map of India with state boundaries
+
+ind1=readRDS("gadm36_IND_1_sp.rds")
+spplot(ind1, "NAME_1", scales=list(draw=T), colorkey=F, main="India")
+
+
+# Map of India with States coloured
+ind1$NAME_1 = as.factor(ind1$NAME_1)
+ind1$fake.data = runif(length(ind1$NAME_1))
+spplot(ind1,"NAME_1",  col.regions=rgb(0,ind1$fake.data,0), colorkey=T, main="Indian States")
+
+
+gujarat1 = (ind1[ind1$NAME_1=="Gujarat",])
+spplot(gujarat1,"NAME_1", col.regions=rgb(0,0,1), 
+       main = "Gujarat, India",scales=list(draw=T), colorkey =F)
+
+
+# Level 2 data from GADM i.e. upto level of district boundaries
+ind2=readRDS("gadm36_IND_2_sp.rds")
+spplot(ind2, "NAME_1", scales=list(draw=T), colorkey=F, main="India")
+
+# Districts of Gujarat
+gujarat2 = (ind2[ind2$NAME_1=="Gujarat",])
+spplot(gujarat2,"NAME_1", main = "Gujarat Districts", colorkey =F)
+
+# Districts of Gujarat Coloured map
+gujarat2$NAME_2 = as.factor(gujarat2$NAME_2)
+col = rainbow(length(levels(gujarat2$NAME_2)))
+spplot(gujarat2,"NAME_2",  col.regions=col, colorkey=T)
+
+# Districts of Gujarat coloued with stipulated fake data 
+gujarat2$NAME_2 = as.factor(gujarat2$NAME_2)
+gujarat2$fake.data = runif(length(gujarat2$NAME_1)) 
+spplot(gujarat2,"NAME_2",  col.regions=rgb(0,gujarat2$fake.data, 0), colorkey=T)
+
+# Colouring the districts with range of colours
+col_no = as.factor(as.numeric(cut(gujarat2$fake.data, c(0,0.2,0.4,0.6,0.8,1))))
+levels(col_no) = c("<20%", "20-40%", "40-60%","60-80%", ">80%")
+gujarat2$col_no = col_no
+myPalette = brewer.pal(5,"Greens")
+spplot(gujarat2, "col_no", col=grey(.9), col.regions=myPalette, main="District Wise Data")
+
+
+# Level 3 Map of India with boundaries of talukas
+ind3=readRDS("gadm36_IND_3_sp.rds")
+spplot(ind3, "NAME_1", scales=list(draw=T), colorkey=F, main="India")
+
+# Map of India with taluka boundaries
+gujarat3 = (ind3[ind3$NAME_1=="Gujarat",])
+
+# Map of Gujarat with talukas coloured
+gujarat3$NAME_3 = as.factor(gujarat3$NAME_3)
+col = rainbow(length(levels(gujarat3$NAME_3)))
+spplot(gujarat3,"NAME_3", main = "Taluk, District - Gujarat", 
+       colorkey=T,col.regions=col,scales=list(draw=T))
+
+# Map of Gandhinagar District with talukas
+gujarat3 = (ind3[ind3$NAME_1=="Gujarat",])
+gandhinagar = (gujarat3[gujarat3$NAME_2=="Gandhinagar",])
+spplot(gandhinagar,"NAME_3", colorkey =F, 
+       scales=list(draw=T), main = "Gandhinagar, Gujarat")
+
+# Map of Gandhinagar taluka
+gandhinagar = (gujarat3[gujarat3$NAME_2=="Gandhinagar",])
+gandhinagar1 = (gandhinagar[gandhinagar$NAME_3=="Gandhinagar",])
+spplot(gandhinagar1,"NAME_3", colorkey =F, scales=list(draw=T),
+       main = "Gandhinagat Taluka")
+
+
+# map of kalol Taluka
+gandhinagar = (gujarat3[gujarat3$NAME_2=="Gandhinagar",])
+kalol = (gandhinagar[gandhinagar$NAME_3=="Kalol",])
+spplot(kalol,"NAME_3", colorkey =F, scales=list(draw=T),
+       main = "Kalol Taluka")
+
+
+
+
+
+
